351f3aa3c8d5ba63dfe84fcc84a78e220b841d4e
MUHAMMED EYÜP YILMAZ <muhammedeyup.yilmaz@student.iu.edu.tr>
2021-07-08 17:01:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ResultViewOfExpandoObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jobList = new object(); /*write here your sql command*/
+            IDictionary<string, object> key;
+            var number = 0;
+            while (number < jobList.Count)
+            {
+                key = jobList[number];
+                var chunk = 0;
+                while (chunk < key.Count)
+                {
+                    Console.WriteLine(key.Values.ToList()[chunk].ToString());
+                    chunk++;
+                }
+                number++;
+            }
+
+        }
+    }
+}
